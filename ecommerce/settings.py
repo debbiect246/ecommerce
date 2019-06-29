27cf698ef931ccd6d4a9106a3ff1d8edc0315100
debbiect246 <deb246@btinternet.com>
2019-06-29 15:01:10 +0100

    authentication backends added to settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.CaseInsensitiveAuth'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
