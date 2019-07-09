e529e1520d52256ef97dfbb5f4f2c0eecf40ff71
debbiect246 <deb246@btinternet.com>
2019-07-09 21:16:28 +0100

    url reset file changed for django 2.2 commands

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import PasswordResetView
+from django.urls import path
 
 urlpatterns = [
+    path(
+        'accounts/password_reset/',
+        PasswordResetView.as_view(),
+        name='password_reset'
+    ),
     url(r'^$', password_reset,
         {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
