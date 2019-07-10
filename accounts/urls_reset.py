f23ef9e2231a5c0887d11d6a02bfe8be074daff3
debbiect246 <deb246@btinternet.com>
2019-07-10 15:31:12 +0100

    trying to sort out app

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -9,10 +9,5 @@ urlpatterns = [
         PasswordResetView.as_view(),
         name='password_reset'
     ),
-    url(r'^$', password_reset,
-        {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
-    url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
-        {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    url(r'^complete/$', password_reset_complete, name='password_reset_complete'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib import messages, auth
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from .forms import UserLoginForm, UserRegistrationForm
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
@@ -31,13 +31,14 @@ def login(request):
                 auth.login(request, user)
                 messages.error(request, "You have successfully logged in")
 
-                if request.GET and request.GET['next'] !='':
+                if request.GET and request.GET['next'] != '':
                     next = request.GET['next']
                     return HttpResponseRedirect(next)
                 else:
                     return redirect(reverse('index'))
             else:
-                user_form.add_error(None, "Your username or password are incorrect")
+                user_form.add_error(
+                    None, "Your username or password are incorrect")
     else:
         user_form = UserLoginForm()
 
